4cc01d1e7cdcfed8089d82549121caf6c15f7822
amresh012 <73485115+amresh012@users.noreply.github.com>
2022-10-28 19:39:55 +0530

    UI update

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-function myfunction() {
-    document.getElementsByClassName(".main").style.backgroundImage = "url('signupandlogin/logo/pexels-andrea-piacquadio-3765112.jpg')";
-};
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+const links = document.querySelectorAll(".nav-links li");
+
+hamburger.addEventListener('click', () => {
+    //Animate Links
+    navLinks.classList.toggle("open");
+    links.forEach(link => {
+        link.classList.toggle("fade");
+    });
+
+    //Hamburger Animation
+    hamburger.classList.toggle("toggle");
+});
